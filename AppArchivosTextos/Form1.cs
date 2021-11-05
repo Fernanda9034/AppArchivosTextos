d6a0be015e706b31b505b79b256ff1d20ce073db
Fernanda9034 <i21050345@monclova.tecnm.mx>
2021-11-04 20:48:06 -0600

    Arreglo en formulario con botones de abrir, insertar y guardar datos.

--- AppArchivosTextos/Form1.cs
+++ AppArchivosTextos/Form1.cs
@@ -20,54 +20,32 @@ namespace AppArchivosTextos
         private void btnAbrir_Click(object sender, EventArgs e)
         {
             {
-                OpenFileDialog dialogo = new OpenFileDialog();
-                OpenFileDialog openFileDialog1 = new OpenFileDialog();
-                openFileDialog1.Filter = "Archivos txt|*.txt";
-                openFileDialog1.FileName = "Seleccione un archivo de Texto";
-                openFileDialog1.Title = "Programa de Lectura";
-                openFileDialog1.InitialDirectory = "C:\\";
-                openFileDialog1.FileName = this.Text;
-                if (openFileDialog1.ShowDialog() == DialogResult.OK)
-                {
-                  this.Text = openFileDialog1.FileName;
-                }
-                    try
-                    {
-                        var filePath = openFileDialog1.FileName;
-                        using (Stream str = openFileDialog1.OpenFile())
-                        { 
-                            this("notepad.exe", filePath);
-                        }
-                    }
-                    catch (SecurityException ex)
-                    {
-                        MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
-                        $"Details:\n\n{ex.StackTrace}");
-                    }
-
-                    //this.textBox1.Text = OpenFileDialog.FileName;
-                    //archivo = StreamReader("documento.txt", "w+");
-                    //MessageBox.Show(dialogo.FileName);
-                    //using (StreamReader archivo = new StreamReader(dialogo.FileName));
                 
-            }
-            //OpenFileDialog openFileDialog1 = new OpenFileDialog();
-            //openFileDialog1.Filter = "Archivos txt|*.txt";
-            //openFileDialog1.FileName = "Seleccione un archivo de Texto";
-            //openFileDialog1.Title = "Programa de Lectura";
-            //openFileDialog1.InitialDirectory = "C:\\";
-            //openFileDialog1.FileName = this.Text;
-            //if (openFileDialog1.ShowDialog() == DialogResult.OK)
-            //{
-            //  this.Text = openFileDialog1.FileName;
-            //}
-            //Text = "";
+                OpenFileDialog openFileDialog1 = new OpenFileDialog();
+               
+                if (openFileDialog1.ShowDialog() != DialogResult.OK)
+                {
+                    return;
+                }
+                string filePath = openFileDialog1.FileName;
+                p = 0;
 
-            //StreamReader sr = new System.IO.StreamReader("", Encoding.Default);//@textBox1.Text, System.Text.Encoding.Default);
-            //string texto;
-            //texto = sr.ReadToEnd();
-            //sr.Close();
-            //Text = texto;
+                // Read the file and display it line by line.  
+                foreach (string line in System.IO.File.ReadLines(filePath))
+                {
+                    string[] datos;
+                    datos = line.Split('-');
+                    Libro n = new Libro();
+                    n.Nombre = datos[0];
+                    n.Precio = double.Parse(datos[1]);
+                    //guardar el libro en la posicion
+                    l[p] = n;
+                    lstArreglo.Items.Add(l[p]);
+                    p++;
+                  
+                }  
+            }
+            
         }
         
 
